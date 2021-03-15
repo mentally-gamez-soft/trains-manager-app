173cb17508366adca42081944007355d94b3f1db
mentally-gamez-soft <mentally.gamez.soft@gmail.com>
2021-03-15 21:05:53 +0100

    Updated environ

--- api_reader/settings.py
+++ api_reader/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '51)-t*g@$7$g7getl2mwv0ycuaq4#5^m9c(_4c+%@yv@%-1o&5'
+# SECRET_KEY = '51)-t*g@$7$g7getl2mwv0ycuaq4#5^m9c(_4c+%@yv@%-1o&5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['trains-manager-app.herokuapp.com']
 
 
 # Application definition
